29eb8a2877964e1f9e42c342bee0bcc09f52f1de
Jandro <jandro.suarez.92@gmail.com>
2017-02-07 10:42:20 +0100

    Practica Examen sin refactorizar

---
+++ PracticaExamen/src/BiciPalma/Estacion.java
@@ -0,0 +1,117 @@
+package BiciPalma;
+
+
+import java.util.concurrent.ThreadLocalRandom;
+public class Estacion {
+	// propiedades
+	private int id = 0;
+	private String direccion = "";
+	private int numeroAnclajes = 0;
+	private Bicicleta[] anclajes;
+
+	// constructores
+	public Estacion(int id, String direccion, int anclaje) {
+		this.id = id;
+		this.direccion = direccion;
+		this.numeroAnclajes = anclaje;
+		this.anclajes = new Bicicleta[anclaje];
+	}
+
+	public void consultarEstacion() {
+		// Devuelve el estado de la estacion, el id, direccion y anclaje
+		System.out.println(getId() + "" + getDireccion() + "" + getAnclajesTotales());
+	}
+
+	// Definimos los metodos que necesitamos para consultar estacion
+	public int getId() {
+		return this.id;
+	}
+
+	public String getDireccion() {
+		return this.direccion;
+	}
+
+	public int getAnclajesTotales() {
+		return this.numeroAnclajes;
+	}
+
+	public Bicicleta[] getAnclajes() {
+		return this.anclajes;
+	}
+	
+	public Bicicleta getAnclaje(int anclaje){
+		return this.anclajes[anclaje];
+	}
+
+	
+
+	// Definimos el metodo que muestra cuantos anclajes libres hay
+	public int anclajesLibres() {
+		int libres = 0;
+		for (int i = 0; i < getAnclajes().length; i++) {
+			if (anclajes[i] == null) {
+				libres += 1;
+			}
+		}
+		return libres;
+	}
+
+	public void anclarBicicleta(Bicicleta bicicleta) {
+
+		for (int i = 0; i < getAnclajes().length; i++) {
+			if (getAnclaje(i) == null) {
+				anclajes[i] = bicicleta;
+				mostrarAnclaje(bicicleta, i);
+				break;
+			}
+		}
+
+	}
+
+	public void mostrarAnclaje(Bicicleta bicicleta, int numeroAnclaje) {
+
+		System.out.println(bicicleta.getId() + " " + numeroAnclaje);
+	}
+
+	public void consultarAnclajes() {
+		for (int i = 0; i < getAnclajes().length; i++) {
+			if (getAnclajes()[i] != null) {
+				mostrarAnclaje(getAnclajes()[i], i);
+			} else {
+				System.out.println("Empty");
+			}
+
+		}
+	}
+
+	public Boolean leerTarjetaUsuario(TarjetaUsuario tarjetaUsuario) {
+		return tarjetaUsuario.isActivada();
+	}
+
+	public void retirarBicicleta(TarjetaUsuario tarjetaUsuario) {
+		if (tarjetaUsuario.isActivada() != false) {
+			int anclajeAleatorio = generarAnclaje();
+			
+				mostrarBicicleta(getAnclajes()[anclajeAleatorio], anclajeAleatorio);
+				getAnclajes()[anclajeAleatorio] = null;
+			}
+			
+
+		}
+
+	
+
+	public int generarAnclaje() {
+		int anclaje = ThreadLocalRandom.current().nextInt(getAnclajes().length-1);
+		while(getAnclajes()[anclaje] == null){
+			anclaje = ThreadLocalRandom.current().nextInt(getAnclajes().length-1);
+		}
+		return anclaje;
+	}
+
+	public void mostrarBicicleta(Bicicleta bicicleta, int numeroAnclaje) {
+		System.out.println(bicicleta.getId() + " " + numeroAnclaje);
+
+	}
+
+}
---
+++ PracticaExamen/src/BiciPalma/TarjetaUsuario.java
@@ -0,0 +1,26 @@
+package BiciPalma;
+
+public class TarjetaUsuario {
+	private String id = "000000000";
+	private Boolean activada = true;
+	
+	public TarjetaUsuario(String id, Boolean estado){
+		this.id = id;
+		this.activada = activada;
+	}
+	
+	public void setActivada(){
+		if(this.activada == true){
+			this.activada = false;
+		}
+		
+		else{
+			this.activada = true;
+		}
+	}
+	
+	public Boolean isActivada(){
+		return this.activada;
+	}
+
+}
